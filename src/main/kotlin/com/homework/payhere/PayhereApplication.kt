3a71ec4260b41e9a998b5928cf91a8d1dd843fbb
MintKim1990 <rlaalsxo1990@gmail.com>
2024-02-14 06:58:14 +0900

    feat: 초기 프로젝트 세팅

---
+++ src/main/kotlin/com/homework/payhere/PayhereApplication.kt
@@ -0,0 +1,11 @@
+package com.homework.payhere
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PayhereApplication
+
+fun main(args: Array<String>) {
+	runApplication<PayhereApplication>(*args)
+}
